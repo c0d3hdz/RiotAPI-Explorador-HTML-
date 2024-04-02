ab8e83996c18a946a69395f8cc69fb82b7f733ae
c0d3hdz <marquezhernandez.damian@gmail.com>
2024-04-01 23:06:44 -0600

    'actualizacsion_en_app'

--- app.js
+++ app.js
@@ -1,49 +1,4 @@
-const apiKey = 'RGAPI-39121669-3362-4807-86cb-5e8a6323d0ff'
-let puuidEncrip = ''
-
-async function getSummonerData(region, summonerName) {
-    const response = await fetch(
-        `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${apiKey}`,
-    )
-    const data = await response.json()
-    return data
-}
-
-async function getChampionMasteries(region, puuidEncrip) {
-    const response = await fetch(
-        `https://${region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/${puuidEncrip}/top?count=3&api_key=${apiKey}`,
-    )
-    const data = await response.json()
-    return data
-}
-
-function displaySummonerData(summoner, region) {
-    const summonerInfo = document.getElementById('summonerInfo')
-    puuidEncrip = summoner.puuid
-    summonerInfo.innerHTML = `
-        <h2>${summoner.name}</h2>
-        <p>Nivel: ${summoner.summonerLevel}</p>
-        <p>Icono de Invocador:</p>
-        <img src="http://ddragon.leagueoflegends.com/cdn/14.6.1/img/profileicon/${summoner.profileIconId}.png" alt="Icono de Invocador">
-    `
-
-    getChampionMasteries(region, puuidEncrip)
-        .then(data => {
-            // Procesar los datos y mostrarlos en la página
-            const championMasteries = data
-                .map(
-                    champion => `
-                <p>Champion: ${champion.championId}, Level: ${champion.championLevel}</p>
-            `,
-                )
-                .join('')
-            summonerInfo.innerHTML += `<div>${championMasteries}</div>`
-        })
-        .catch(error => {
-            console.error('Error al obtener las maestrías de campeones:', error)
-            summonerInfo.innerHTML += `<p>Error al obtener las maestrías de campeones.</p>`
-        })
-}
+const apiKey = 'RGAPI-bb3a1d7d-e5ee-4fe8-b378-30c7b2a05f07' // API_KEY
 
 async function searchSummoner() {
     const region = document.getElementById('region').value
@@ -62,4 +17,84 @@ async function searchSummoner() {
             'Error al obtener datos del invocador. Por favor revisa el nombre ingresado y asegúrate de que sea correcto.',
         )
     }
+}
+
+async function getSummonerData(region, summonerName) {
+    const response = await fetch(
+        `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${apiKey}`,
+    )
+    const data = await response.json()
+    return data
+}
+
+async function getChampionMasteries(region, puuidEncrip) {
+    const response = await fetch(
+        `https://${region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/${puuidEncrip}/top?count=3&api_key=${apiKey}`,
+    )
+    const data = await response.json()
+    return data
+}
+
+async function getChampionData() {
+    const response = await fetch('http://ddragon.leagueoflegends.com/cdn/14.6.1/data/en_US/champion.json')
+    const data = await response.json()
+    return data.data
+}
+
+async function displayChampionMasteries(region, puuidEncrip) {
+    const summonerInfo = document.getElementById('summonerInfo')
+    try {
+        const response = await fetch(
+            `https://${region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/${puuidEncrip}/top?count=3&api_key=${apiKey}`,
+        )
+        const championMasteries = await response.json()
+        const championData = await getChampionData()
+
+        championMasteries.forEach(championMastery => {
+            const championId = championMastery.championId
+            const championName = Object.keys(championData).find(key => championData[key].key == championId)
+            const championImageUrl = `http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${championName}_0.jpg`
+
+            const championContainer = document.createElement('div')
+            championContainer.classList.add('champion-container')
+
+            const championImage = document.createElement('img')
+            championImage.src = championImageUrl
+            championImage.alt = championName
+            championImage.classList.add('champion-image')
+
+            const championNameElement = document.createElement('p')
+            championNameElement.textContent = championName
+            championNameElement.classList.add('champion-name')
+
+            championContainer.appendChild(championImage)
+            championContainer.appendChild(championNameElement)
+
+            summonerInfo.appendChild(championContainer)
+        })
+    } catch (error) {
+        console.error('Error al obtener las maestrías de campeones:', error)
+        summonerInfo.innerHTML += `<p>Error al obtener las maestrías de campeones.</p>`
+    }
+}
+
+function displaySummonerData(summoner, region) {
+    const summonerInfo = document.getElementById('summonerInfo')
+    let puuidEncrip = summoner.puuid
+    summonerInfo.innerHTML = ''
+
+    const summonerName = document.createElement('h2')
+    summonerName.textContent = summoner.name
+    summonerInfo.appendChild(summonerName)
+
+    const summonerIcon = document.createElement('img')
+    summonerIcon.src = `http://ddragon.leagueoflegends.com/cdn/14.6.1/img/profileicon/${summoner.profileIconId}.png`
+    summonerIcon.alt = 'Icono de Invocador'
+    summonerInfo.appendChild(summonerIcon)
+
+    const summonerLevel = document.createElement('p')
+    summonerLevel.textContent = `Nivel: ${summoner.summonerLevel}`
+    summonerInfo.appendChild(summonerLevel)
+
+    displayChampionMasteries(region, puuidEncrip)
 }
